208966368bd0f3f47a46d367e5cbc925d48a8a48
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2024-08-12 23:03:12 +0200

    Set up admin claims and protected endpoints

--- src/api/users.py
+++ src/api/users.py
@@ -1,19 +1,26 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt
+from flask_cors import CORS, cross_origin
 from data.user_model import User
 from utils.json_schemas import UserSchema
-
 
 users_bp = Blueprint('users', __name__)
 
 @users_bp.get('/all')
 @jwt_required()
 def get_all_users():
+    claims = get_jwt()
+    if claims.get('role') != 'admin':
+        return jsonify({
+            'message': 'You are not authorized to access this resource',
+            'error': 'unauthorized'
+        }), 403
+    
     page = request.args.get(
         'page', 
         default=1, 
         type=int
-    ) # Dict method, default is 1
+    )
     
     per_page = request.args.get(
         'per_page', 
@@ -30,4 +37,73 @@ def get_all_users():
     
     return jsonify({
         "users": result,
+    }), 200
+    
+@users_bp.post('/delete')
+@jwt_required()
+def delete_user():
+    claims = get_jwt()
+    if claims.get('role') != 'admin':
+        return jsonify({
+            'message': 'You are not authorized to access this resource',
+            'error': 'unauthorized'
+        }), 403
+    
+    username = request.args.get('username')
+    user = User.get_user_by_username(username)
+    
+    if not user:
+        return jsonify({
+            'message': 'User not found',
+            'error': 'not_found'
+        }), 404
+        
+    user.delete()
+    
+    return jsonify({
+        'message': 'User deleted successfully',
+    }), 200
+    
+@users_bp.post('/update-password')
+@jwt_required()
+def update_password():
+    data = request.get_json()
+    requested_username = data.get(
+        'username', 
+        None
+    )
+    claims = get_jwt()
+    new_password = request.json.get('new_password')
+    
+    # Admin can update any user's password
+    if requested_username and claims.get('role') == 'admin':
+        user = User.get_user_by_username(requested_username)
+        
+        if not user:
+            return jsonify({
+                'message': 'User not found',
+                'error': 'not_found'
+            }), 404
+            
+        user.set_password(new_password)
+        user.save()
+        return jsonify({
+            'message': 'Password updated successfully',
+        }), 200
+    
+    # User can update his/her password
+    claimed_username = claims.get('sub')
+    user = User.get_user_by_username(claimed_username)
+    
+    if not user:
+        return jsonify({
+            'message': 'User not found',
+            'error': 'not_found'
+        }), 404
+    
+    user.set_password(new_password)
+    user.save()
+        
+    return jsonify({
+        'message': 'Password updated successfully',
     }), 200
--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from api.auth import auth_bp
 from api.users import users_bp
 from api.ai_pdf_assistant import ai_pdf_assistant_bp
 from flask_cors import CORS, cross_origin
+import logging
+from flask_jwt_extended import jwt_required, get_jwt
+from data.user_model import User
 
 # TODO: Move extensions in another place
 # TODO: Fix the sha256 in db
@@ -15,9 +18,50 @@ def create_app():
     db.init_app(app)
     jwt.init_app(app)
     
-    # Register the blueprints
+    # Register the blueprints  
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(users_bp, url_prefix='/users')
     app.register_blueprint(ai_pdf_assistant_bp, url_prefix='/ai')
     
+    # Additional claims loader 
+    @jwt.additional_claims_loader
+    def add_claims_to_access_token(identity):
+        if identity == 'fatjonfreskina':
+            return { 'role': 'admin' }
+        return { 'role': 'user'}
+    
+    # User loader
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_headers, jwt_data):
+        identity = jwt_data["sub"]
+        return User.query.filter_by(username=identity).one_or_none()
+    
+    # Error handling 
+    @jwt.expired_token_loader
+    def my_expired_token_callback(jwt_header, jwt_data):
+        return jsonify({
+            'message': 'The token has expired',
+            'error': 'token_expired' 
+        }), 401
+    
+    @jwt.invalid_token_loader
+    def my_invalid_token_callback(error):
+        return jsonify({
+            'message': 'Signature verification failed',
+            'error': 'invalid_token' 
+        }), 401
+        
+    @jwt.unauthorized_loader
+    def my_unauthorized_loader(error):
+        return jsonify({
+            'message': 'Missing Authorization Header',
+            'error': 'authorization_required' 
+        }), 401
     return app
+
+
+@auth_bp.get('/whoami')
+@jwt_required()
+def whoami():
+    claims = get_jwt()
+    return jsonify( { 'message': 'You are authenticated', 'claims': claims } )
